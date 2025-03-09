8912db91365e433eff260cd5b86589c3875aca49
Armaangg <newholland3639@gmail.com>
2025-03-09 19:02:05 +0530

    Update status.py

--- status.py
+++ status.py
@@ -7,23 +7,15 @@ from flask import Flask
 app = Flask(__name__)
 
 WEBHOOK_URL = 'https://discord.com/api/webhooks/1348251353176211466/aJuTBKtxqOUCbHqOTamKZsuP4EmWDofZqq9o9FFZvbyuczL5JwhZHmBcAIc4SKKA9Z3b'
+MESSAGE_ID = '1348286214284906618'
 API_URL = 'http://fi1.bot-hosting.net:6957/'
 
-status_message_id = None
 uptime = 0
 downtime = 0
 
-def send_message(embed):
-    global status_message_id
-    response = requests.post(WEBHOOK_URL, json={"embeds": [embed]})
-    if response.status_code == 200:
-        status_message_id = response.json()['id']
-    return response
-
 def edit_message(embed):
-    if status_message_id:
-        response = requests.patch(f"{WEBHOOK_URL}/messages/{status_message_id}", json={"embeds": [embed]})
-        return response
+    response = requests.patch(f"{WEBHOOK_URL}/messages/{MESSAGE_ID}", json={"embeds": [embed]})
+    return response
 
 def build_embed(status):
     now = datetime.now(pytz.timezone('Asia/Kolkata')).strftime('%Y-%m-%d %I:%M:%S %p')
@@ -50,19 +42,19 @@ def home():
             if response.status_code == 200:
                 if previous_status != 'Online':
                     embed = build_embed('Online')
-                    send_message(embed)
+                    edit_message(embed)
                 previous_status = 'Online'
                 uptime += 1
             else:
                 if previous_status != 'Offline':
                     embed = build_embed('Offline')
-                    send_message(embed)
+                    edit_message(embed)
                 previous_status = 'Offline'
                 downtime += 1
         except:
             if previous_status != 'Offline':
                 embed = build_embed('Offline')
-                send_message(embed)
+                edit_message(embed)
             previous_status = 'Offline'
             downtime += 1
         
